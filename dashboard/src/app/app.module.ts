974fc4a36f05e550e9d2caf995e6b2d0a7cf5df6
Joao Eduardo Luis <joao@wipwd.dev>
2021-06-06 10:37:48 +0000

    dashboard: add material design icons
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +13,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  public constructor(
+    matIconRegistry: MatIconRegistry,
+    domSanitizer: DomSanitizer
+  ) {
+    matIconRegistry.addSvgIconSet(
+      domSanitizer.bypassSecurityTrustResourceUrl("./assets/mdi.svg")
+    );
+  }
+}
