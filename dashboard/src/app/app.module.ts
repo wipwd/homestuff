0724987e0007f57d327ecb7bc1e649a97d8e2952
Joao Eduardo Luis <joao@wipwd.dev>
2021-06-06 11:14:35 +0000

    dashboard: add eslint
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
