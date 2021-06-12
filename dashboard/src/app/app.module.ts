00ab7887fa53093d00d8b68b98fd2230fcd5c075
Joao Eduardo Luis <joao@wipwd.dev>
2021-06-12 16:03:24 +0000

    dashboard: add licensing headers
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,3 +1,17 @@
+/*
+ * home stuff
+ * Copyright (C) 2021  Joao Eduardo Luis <joao@wipwd.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
