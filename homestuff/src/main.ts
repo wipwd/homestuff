17fabc66bbbb407093dedb46307ea174b66578a0
Joao Eduardo Luis <joao@wipwd.dev>
2021-06-12 23:26:39 +0000

    hs: move api endpoints to '/api'
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- homestuff/src/main.ts
+++ homestuff/src/main.ts
@@ -12,21 +12,22 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  */
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import { AppModule } from './app.module';
-import { ZwaveService } from './zwave/zwave.service';
+import { AppModule } from "./app.module";
+import { ZwaveService } from "./zwave/zwave.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
 
   const config = new DocumentBuilder()
-  .setTitle("Home Stuff")
-  .setDescription("Home Stuff API Description")
-  .setVersion("1.0")
-  .build();
+    .setTitle("Home Stuff")
+    .setDescription("Home Stuff API Description")
+    .setVersion("1.0")
+    .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup("api/docs", app, document);
 
   await app.listen(31337);
 
